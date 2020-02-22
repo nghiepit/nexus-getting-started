700aa4a2f8f778af6342472fa5ae7c1871781748
nghiepit <me@nghiepit.dev>
2020-02-22 12:51:34 +0700

    Remove commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es6: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -10,6 +14,7 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
+    'no-console': 'warn',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/camelcase': 'off',
